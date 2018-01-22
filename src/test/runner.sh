abd0675a6b37eeafd9c7a08aaacedb739bfa6d88
Nick Clark <nicholas.clark@gmail.com>
2018-01-22 08:52:01 -0700

    Bugfixes to test runner

--- src/test/runner.sh
+++ src/test/runner.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 TARGET=$(echo $0 | sed 's@.*/@@g' | sed 's@^test-@@g')
 ctmp=$(realpath $ctmp)
 echo $TARGET
 RETVAL=0
 
-"$TESTSRC_DIR/$TARGET.check" 1>"$TARGET.stdout" 2>"$TARGET.stderr"
+"$TESTSRC_DIR/$TARGET.check" 2>&1 | cat -v >"$TARGET.stdout"
 
 if ! diff -q "$TARGET.stdout" "$TESTSRC_DIR/$TARGET.dist"; then
-    cat "$TARGET.stderr" >&2
     echo "test for '$TARGET' failed!" >&2
+    echo "------ diff: ------" >&2
+    diff -u "$TARGET.stdout" "$TESTSRC_DIR/$TARGET.dist"  --color >&2
     RETVAL=1
 fi
 
